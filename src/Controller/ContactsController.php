0cc79329616724fcfa3455258735f43481afe978
jendavales <jenda.vales@seznam.cz>
2022-02-28 09:50:35 +0100

    project setup

---
+++ src/Controller/ContactsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactsController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+}
